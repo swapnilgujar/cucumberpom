cd348343d97ba8c37599a2d57feb74ccba6f4243
swapnil gujar <swapnilgujar@hotmail.com>
2023-08-29 04:37:06 +0000

    first commint

---
+++ src/test/java/com/runners/TestRunner.java
@@ -0,0 +1,35 @@
+package com.runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+		features = "src//test//resources//taggedfeature//",
+		glue = {"stepdefinition"},
+		monochrome = true,
+		dryRun = true,
+//		tags = "@SmokeTest or @RegressionTest",
+//		tags = "@SmokeTest and @RegressionTest",
+//		tags = "@SmokeTest",
+//		tags = "@RegressionTest",
+//		tags = "not @RegressionTest",
+		tags= "@PhaseTwo and @SmokeTest",
+		plugin = { /* "pretty", */
+				"html:target/reports/html/HtmlReport.html",
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+				"rerun:target/failedScenarios.txt"
+		/*
+		 * "usage:target/reports/usage/UsageReport",
+		 * "json:target/reports/json/JsonReport.report",
+		 * "usage:target/reports/junit/UnitReport.xml"
+		 */}
+		)
+
+public class TestRunner extends AbstractTestNGCucumberTests {
+	
+	//for paraller execution
+	/*
+	 * @DataProvider(parallel= true) public Object[][]scenarios(){ return
+	 * super.scenarios(); }
+	 */
+}
